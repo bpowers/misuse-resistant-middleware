22671a1785fd2daf06adc1550aa1f391cc1b266e
Bobby Powers <bobbypowers@gmail.com>
2021-10-07 18:16:38 -0700

    basic server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module goedge
 
 go 1.18
+
+require goji.io v2.0.2+incompatible
--- main.go
+++ main.go
@@ -1,4 +1,60 @@
 package main
 
+import (
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"goji.io"
+	"goji.io/pat"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	name := pat.Param(r, "name")
+	fmt.Fprintf(w, "Hello, %s!\n", name)
+}
+
 func main() {
+	addr := flag.String("addr", "localhost:443", "address to listen on")
+	certFile := flag.String("cert", "localhost.pem", "TLS certificate file")
+	keyFile := flag.String("key", "localhost-key.pem", "TLS private key file")
+	flag.Parse()
+
+	mux := goji.NewMux()
+	mux.HandleFunc(pat.Get("/hello/:name"), hello)
+
+	server := &http.Server{
+		Addr: *addr,
+		Handler: mux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  15 * time.Second,
+		TLSConfig: &tls.Config{
+			// Causes servers to use Go's default ciphersuite preferences,
+			// which are tuned to avoid attacks. Does nothing on clients.
+			PreferServerCipherSuites: true,
+			// Only use curves which have assembly implementations
+			CurvePreferences: []tls.CurveID{
+				tls.CurveP256,
+				tls.X25519,
+			},
+			MinVersion: tls.VersionTLS12,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			},
+		},
+	}
+
+	log.Printf("serving on addr %s\n", *addr)
+	if err := server.ListenAndServeTLS(*certFile, *keyFile); err != nil {
+		log.Fatalf("ListenAndServeTLS: %s\n", err)
+	}
 }
