886d61d1980f5dcf66b68dd8ea2e1262341f3d79
Bobby Powers <bobbypowers@gmail.com>
2021-10-07 20:45:59 -0700

    more comments

--- middleware/b/b.go
+++ middleware/b/b.go
@@ -7,10 +7,10 @@ import (
 	"goedge/middleware/a"
 )
 
-// NewEchoNameHandler echos the users name, extracted by the NameExtractorMiddleware.
+// NewEchoNameHandler echos the users name -- it depends on NameExtractorMiddleware running before it.
 func NewEchoNameHandler(key a.NameKey) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		name := a.GetName(req.Context(), key)
-		fmt.Fprintf(rw, "Hello, %s!\n", name)
+		_, _ = fmt.Fprintf(rw, "Hello, %s!\n", name)
 	})
 }
